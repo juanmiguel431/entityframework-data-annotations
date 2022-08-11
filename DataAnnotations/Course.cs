f80f829d4acc642bc741548f42bb303ead34bdb9
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-10 22:06:08 -0400

    AddAnnotationsToCourseTable migration added.

--- DataAnnotations/Course.cs
+++ DataAnnotations/Course.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataAnnotations
 {
@@ -11,14 +13,19 @@ namespace DataAnnotations
 
         public int Id { get; set; }
 
+        [Required, MaxLength(255)]
         public string Name { get; set; }
 
+        [Required, MaxLength(2000)]
         public string Description { get; set; }
 
         public int Level { get; set; }
 
         public float FullPrice { get; set; }
 
+        [ForeignKey(nameof(Author))]
+        public int AuthorId { get; set; }
+        
         public virtual Author Author { get; set; }
         public virtual ICollection<Tag> Tags { get; set; }
     }
