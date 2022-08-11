dedbe262abac5a98654093ed1fb46ab925fae2af
juanmiguel431 <juanmiguel431@gmail.com>
2022-08-10 21:45:05 -0400

    Add project files.

---
+++ DataAnnotations/PlutoContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace DataAnnotations
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+    }
+}
